9b969ed592905a60ce10899a7c2354b39848f6f1
area-man <thefucci@gmail.com>
2014-10-11 00:29:11 -0700

    simplified extents().R

--- extents().R
+++ extents().R
@@ -1,14 +1,12 @@
 #convenient and quick way to get x and y extents from a data frame
-extents <- function(table,y_id,x_id) { 
-  y <- which(colnames(table)==y_id)
-  x <- which(colnames(table)==x_id)
-  ymin <- min(table[y])
-  ymax <- max(table[y])
-  xmin <- min(table[x])
-  xmax <- max(table[x])
+extents <- function(y_id,x_id) { 
+  ymin <- min(y_id)
+  ymax <- max(y_id)
+  xmin <- min(x_id)
+  xmax <- max(x_id)
   data.frame(ymin=ymin,ymax=ymax,xmin=xmin,xmax=xmax)
 }
 
-#extents() returns a data frame of x-y extents so you can do stuff like...
+#extents() returns a data frame of x-y extents, e.g.
 #lims <- extents(atl_boundary,"lat","long")
 #atl_osm <- corner_bbox(lims$xmin, lims$ymin, lims$xmax, lims$ymax)
